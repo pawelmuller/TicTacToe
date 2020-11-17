5345a4724cad12e0154f5bba840fc1d6ae7fad0d
Paweł <pawelmuller42@gmail.com>
2020-11-17 12:10:24 +0100

    Added class TicTacToe that will handle the game.
    Added print_board method that shows user-friendly board on the screen.

--- main.py
+++ main.py
@@ -1,2 +1,34 @@
-if __name__ == "__main__":
+class TicTacToe:
+    def __init__(self):
+        '''self._board = [[0, 0, 0],
+                       [0, 0, 0],
+                       [0, 0, 0]]'''
+
+        self._board = [[2, 2, 0],
+                       [1, 2, 1],
+                       [0, 1, 0]]
+        self._characters = [' ', '◯', '✕']
+
+    def print_board(self):
+        output = ""
+        upper_frame = "┏━━━┳━━━┳━━━┓\n"
+        inner_frame = "┣━━━╋━━━╋━━━┫\n"
+        bottom_frame = "┗━━━┻━━━┻━━━┛\n"
+
+        output += upper_frame
+        row_counter = 0
+        for row in self._board:
+            for element in row:
+                output += f"┃ {self._characters[element]} "
+            row_counter += 1
+            output += f"┃\n{inner_frame if row_counter < 3 else bottom_frame}"
+        print(f"{output:^80}")
+
+
+def minimax():
     pass
+
+
+if __name__ == "__main__":
+    game = TicTacToe()
+    game.print_board()

112f3ea8e7742ade0ec7aa4a006f1de93a1e27d8
Paweł <pawelmuller42@gmail.com>
2020-11-17 17:42:34 +0100

    Changed naming, added Node class prototype.

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ class TicTacToe:
                        [0, 0, 0],
                        [0, 0, 0]]'''
 
-        self._board = [[2, 2, 0],
-                       [1, 2, 1],
+        self._board = [[-1, -1, 0],
+                       [1, -1, 1],
                        [0, 1, 0]]
         self._characters = [' ', '◯', '✕']
 
@@ -13,7 +13,7 @@ class TicTacToe:
         output = ""
         upper_frame = "┏━━━┳━━━┳━━━┓\n"
         inner_frame = "┣━━━╋━━━╋━━━┫\n"
-        bottom_frame = "┗━━━┻━━━┻━━━┛\n"
+        lower_frame = "┗━━━┻━━━┻━━━┛\n"
 
         output += upper_frame
         row_counter = 0
@@ -21,8 +21,24 @@ class TicTacToe:
             for element in row:
                 output += f"┃ {self._characters[element]} "
             row_counter += 1
-            output += f"┃\n{inner_frame if row_counter < 3 else bottom_frame}"
-        print(f"{output:^80}")
+            output += f"┃\n{inner_frame if row_counter < 3 else lower_frame}"
+        print(output)
+
+    def get_possible_moves(self):
+        possibilities = []
+        for y in range(3):
+            for x in range(3):
+                if self._board[2-y][x] != 0:
+                    possibilities.append((x, y))
+        return possibilities if possibilities else False
+
+
+class Node:
+    def __init__(self):
+        self._children = []
+
+    def is_terminal(self):
+        pass
 
 
 def minimax():
@@ -32,3 +48,4 @@ def minimax():
 if __name__ == "__main__":
     game = TicTacToe()
     game.print_board()
+
